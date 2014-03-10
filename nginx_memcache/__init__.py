d976a8c98d3337013f0bb4c939e7c12c7ad7a699
Marc Pomar <marc@faable.com>
2014-03-10 11:51:45 +0100

    Fix version string build that causes setup.py to fail

--- nginx_memcache/__init__.py
+++ nginx_memcache/__init__.py
@@ -1,8 +1,8 @@
-VERSION = (0, 1)
-
+VERSION = (
+	0 , # Major 
+	1 , # Minor
+	1 ) # Patches or Sprint version
 
 def get_version():
-    version = '%s.%s' % (VERSION[0], VERSION[1])
-    if VERSION[2]:
-        version = '%s.%s' % (version, VERSION[2])
-    return version
+	version = ".".join(map(str, VERSION))
+	return version
